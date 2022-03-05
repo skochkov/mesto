0cacbf34bc87ac49cc1598c9e38ee968ab166d13
skochkov <levsha.38@yandex.ru>
2022-03-05 18:23:24 +0800

    fix: review3_8PR

--- src/components/FormValidator.js
+++ src/components/FormValidator.js
@@ -1,5 +1,5 @@
 export default class FormValidator {
-    constructor(config, form) {
+    constructor(config, formElement) {
         this._formSelector = config.formSelector
         this._inputSelector = config.inputSelector
         this._submitButtonSelector = config.submitButtonSelector
@@ -7,7 +7,7 @@ export default class FormValidator {
         this._inputErrorClass = config.inputErrorClass
         this._errorClass = config.errorClass
 
-        this._form = form
+        this._form = document.querySelector(formElement)
 
         this._inputList = Array.from(this._form.querySelectorAll(this._inputSelector))
         this._buttonElement = this._form.querySelector(this._submitButtonSelector);
